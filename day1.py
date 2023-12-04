8d8d2676ccf4d7041d2c59536be13c59c8dfa457
abe545 <aheidebrecht@rippling.com>
2023-12-04 09:21:53 -0500

    Do it in one file

--- day1.py
+++ day1.py
@@ -1,6 +1,12 @@
+from day1_input import real_input
 from itertools import count
 
-example_input = """two1nine
+part1_example_input = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
+
+part2_example_input = """two1nine
 eightwothree
 abcone2threexyz
 xtwone3four
@@ -9,6 +15,17 @@ zoneight234
 7pqrstsixteen"""
 
 _numberMap = dict(zip("one two three four five six seven eight nine".split(), count(1)))
+
+def part1(input_string: str = part1_example_input):
+    lines = input_string.split('\n')
+    for line in map(_ints, lines):
+        yield line[0] * 10 + line[-1]
+
+
+def _ints(input_string: str):
+    return list(int(c) for c in input_string if c.isdigit())
+
+
 def maybe_integer(input_string: str, *, from_end: bool = False):
     if not len(input_string):
         return
@@ -21,22 +38,26 @@ def maybe_integer(input_string: str, *, from_end: bool = False):
     elif input_string[0].isdigit():
         return int(input_string[0])
 
-    for k,v in _numberMap.items():
+    for k, v in _numberMap.items():
         if find(input_string, k):
             return v
 
-def get_digits_part2(input_string: str = example_input):
+
+def get_digits_part2(input_string: str = part2_example_input):
     lines = input_string.splitlines()
     for line in lines:
-        first_digit=None
-        second_digit=None
+        first_digit = None
+        second_digit = None
         line_len = len(line)
         for i in range(line_len):
             first_digit = maybe_integer(line[i:])
             if first_digit is not None:
                 break
         for i in range(line_len):
-            second_digit = maybe_integer(line[:line_len-i], from_end=True)
+            second_digit = maybe_integer(line[:line_len - i], from_end=True)
             if second_digit is not None:
                 break
-        yield 10*first_digit+second_digit
+        yield 10 * first_digit + second_digit
+
+
+print(sum(part1(real_input)), sum(get_digits_part2(real_input)))
