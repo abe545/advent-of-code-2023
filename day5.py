770f30722fae81ba0c892bc846df3cbee6a43d01
abe545 <aheidebrecht@rippling.com>
2023-12-06 17:34:59 -0500

    day 5 part 2

--- day5.py
+++ day5.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from itertools import accumulate
 from day5_input import actual_input
 
 example_input = """seeds: 79 14 55 13
@@ -45,41 +44,58 @@ class AlmanacRanges:
     @classmethod
     def from_str(cls, s: str):
         ranges = [AlmanacRange.from_str(x) for x in s.splitlines()[1:]]
+        ranges.sort(key=lambda ar: ar.source.start)
         return AlmanacRanges(ranges)
 
-    def get_next(self, item: int):
-        for r in self.ranges:
-            if r.in_range(item):
-                return r.destination_start + item - r.source_start
-        return item
+    def map_range(self, rs: list[range]) -> list[range]:
+        acc = []
+        while rs:
+            r = rs.pop()
+            found_overlap = False
+            for known_range in self.ranges:
+                if found_overlap := known_range.is_overlapping(r):
+                    if r.start >= known_range.source.start and r.stop <= known_range.source.stop:
+                        # the range is fully contained in the known range, so offset it and add it to the result
+                        acc.append(range(r.start + known_range.offset, r.stop + known_range.offset))
+                    elif r.start < known_range.source.start:
+                        # the range partially overlaps the start of the known range, so add the portion in the
+                        # known range to the result (after offsetting it)
+                        # add the remainder back to rs, so we can process it in future iterations
+                        acc.append(range(known_range.source.start + known_range.offset, r.stop + known_range.offset))
+                        rs.append(range(r.start, known_range.source.start))
+                    elif r.stop > known_range.source.stop:
+                        # the range partially overlaps the end of the known range, so add the portion in the
+                        # known range to the result (after offsetting it)
+                        # add the remainder back to rs, so we can process it in future iterations
+                        acc.append(range(r.start + known_range.offset, known_range.source.stop + known_range.offset))
+                        rs.append(range(known_range.source.stop, r.stop))
+                    break
+            if not found_overlap:
+                # no known overlapping ranges, so just add it as-is to the results
+                acc.append(r)
+
+        return acc
 
 @dataclass
 class AlmanacRange:
-    source_start: int
-    destination_start: int
-    source_end: int
+    source: range
+    offset: int
 
     @classmethod
     def from_str(cls, s: str):
         parts = s.split()
-        source_range_start = int(parts[1])
-        destination_range_start = int(parts[0])
+        source_start = int(parts[1])
+        destination_start = int(parts[0])
         count = int(parts[2])
-        return AlmanacRange(source_range_start, destination_range_start, source_range_start+count)
+        return AlmanacRange(range(source_start, source_start+count), destination_start - source_start)
 
-    def in_range(self, value: int):
-        return self.source_start <= value <= self.source_end
+    def is_overlapping(self, value: range):
+        return value.start in self.source or value.stop - 1 in self.source
 
 @dataclass
 class Almanac:
     seeds: list[int]
-    seed_to_soil_map: AlmanacRanges
-    soil_to_fertilizer_map: AlmanacRanges
-    fertilizer_to_water_map: AlmanacRanges
-    water_to_light_map: AlmanacRanges
-    light_to_temperature_map: AlmanacRanges
-    temperature_to_humidity_map: AlmanacRanges
-    humidity_to_location_map: AlmanacRanges
+    ranges: list[AlmanacRanges]
 
     @classmethod
     def from_str(cls, s: str) -> Almanac:
@@ -95,30 +111,43 @@ class Almanac:
 
         return Almanac(
             seeds,
-            seed_to_soil_map,
-            soil_to_fertilizer_map,
-            fertilizer_to_water_map,
-            water_to_light_map,
-            light_to_temperature_map,
-            temperature_to_humidity_map,
-            humidity_to_location_map,
+            [
+                seed_to_soil_map,
+                soil_to_fertilizer_map,
+                fertilizer_to_water_map,
+                water_to_light_map,
+                light_to_temperature_map,
+                temperature_to_humidity_map,
+                humidity_to_location_map,
+            ],
         )
 
-    def get_location_numbers(self) -> list[int]:
-        ranges = [
-            self.seed_to_soil_map,
-            self.soil_to_fertilizer_map,
-            self.fertilizer_to_water_map,
-            self.water_to_light_map,
-            self.light_to_temperature_map,
-            self.temperature_to_humidity_map,
-            self.humidity_to_location_map,
-        ]
-        return [[x for x in accumulate(ranges, lambda acc, d: d.get_next(acc), initial=seed)][-1] for seed in self.seeds]
+    def _get_min_seed_location(self, seed_ranges: list[range]) -> list[int]:
+        cur = seed_ranges
+        for r in self.ranges:
+            cur = r.map_range(cur)
+        return min(map(lambda r: r.start, cur))
+
+    def get_min_location_number(self) -> int:
+        return min(self._get_min_seed_location([range(s, s)]) for s in self.seeds)
+
+    def get_min_location_number_if_seeds_are_ranges(self):
+        acc = []
+        i = 0
+        while i < len(self.seeds):
+            seed_start = self.seeds[i]
+            seed_count = self.seeds[i + 1]
+            acc.append(self._get_min_seed_location([range(seed_start, seed_start + seed_count - 1)]))
+            i += 2
+        return min(acc)
 
 def part1(s=example_input):
     almanac = Almanac.from_str(s)
-    return min(almanac.get_location_numbers())
+    return almanac.get_min_location_number()
+
+def part2(s=example_input):
+    almanac = Almanac.from_str(s)
+    return almanac.get_min_location_number_if_seeds_are_ranges()
 
 
-print(part1(actual_input))
+print(part1(actual_input), part2(actual_input))
