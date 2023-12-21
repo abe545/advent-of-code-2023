b12fb50cadfb6bf424da5a5eed1947867aa6a6b8
abe545 <aheidebrecht@rippling.com>
2023-12-21 14:28:23 -0500

    Day9 part 2

--- day9.py
+++ day9.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from day9_input import actual_input
+from shared import timer
 
 example_input = """0 3 6 9 12 15
 1 3 6 10 15 21
@@ -21,12 +22,17 @@ class OasisHistory:
             if (i == 0):
                 continue
             yield v - seq[i-1]
-    def find_next_digit(self):
+
+    def get_steps(self):
         seq = self.sequence
         steps = [seq]
         while len(set(seq)) > 1:
             seq = list(self._diff(seq))
             steps.append(seq)
+        return steps
+
+    def find_next_digit(self):
+        steps = self.get_steps()
 
         while len(steps) > 1:
             seq = steps.pop()
@@ -35,6 +41,17 @@ class OasisHistory:
             n.append(n[-1] + diff)
 
         return steps[-1][-1]
+
+    def find_previous_digit(self):
+        steps = self.get_steps()
+
+        while len(steps) > 1:
+            seq = steps.pop()
+            diff = seq[0]
+            n = steps[-1]
+            n.insert(0, n[0] - diff)
+
+        return steps[-1][0]
 
 @dataclass
 class OasisHistories:
@@ -45,9 +62,15 @@ class OasisHistories:
         sequences = [OasisHistory.from_str(s) for s in s.splitlines()]
         return OasisHistories(sequences)
 
+@timer
 def part1(s: str = example_input):
     oh = OasisHistories.from_str(s)
     return sum(map(OasisHistory.find_next_digit, oh.sequences))
 
+@timer
+def part2(s: str = example_input):
+    oh = OasisHistories.from_str(s)
+    return sum(map(OasisHistory.find_previous_digit, oh.sequences))
 
-print(part1(actual_input))
+
+print(part1(actual_input), part2(actual_input))
